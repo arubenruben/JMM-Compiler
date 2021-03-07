6188e4aca66e272eb4792540b43a0cee61a98272
LockdownPT <guerra70@live.com.pt>
2021-03-07 16:55:28 +0000

    Trying to eliminate error codes in main function/class

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,33 @@ import pt.up.fe.comp.jmm.JmmParser;
 import pt.up.fe.comp.jmm.JmmParserResult;
 import pt.up.fe.comp.jmm.report.Report;
 
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.io.StringReader;
 
 public class Main implements JmmParser {
 
+	static String content = "";
+	static JmmParserResult result = null;
 
 	public JmmParserResult parse(String jmmCode) {
 		
 		try {
-		    Calculator myCalc = new Calculator(new StringReader(jmmCode));
-    		SimpleNode root = myCalc.Expression(); // returns reference to root node
+		    Parser parser = new Parser(new StringReader(jmmCode));
+    		SimpleNode root = parser.Program(); // returns reference to root node
             	
     		root.dump(""); // prints the tree on the screen
+
+
     	
     		return new JmmParserResult(root, new ArrayList<Report>());
 		} catch(ParseException e) {
 			throw new RuntimeException("Error while parsing", e);
 		}
+
 	}
 
     public static void main(String[] args) {
@@ -29,6 +37,14 @@ public class Main implements JmmParser {
         if (args[0].contains("fail")) {
             throw new RuntimeException("It's supposed to fail");
         }
+
+        Main main = new Main();
+
+		try {
+			 main.parse(Files.readString(Paths.get(args[0])));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 
 
