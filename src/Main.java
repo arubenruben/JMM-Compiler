995d2bc773a31c9b814dcd8c32682a947c37ec13
Ruben Almeida <almeidaruben35@gmail.com>
2021-03-07 21:02:01 +0000

    Parser starts with proved fails in the example with IFs and with main array declaration

--- src/Main.java
+++ src/Main.java
@@ -10,22 +10,22 @@ import java.util.Arrays;
 
 public class Main implements JmmParser {
 
-	static String content = "";
-	static JmmParserResult result = null;
+    static String content = "";
+    static JmmParserResult result = null;
 
-	public JmmParserResult parse(String jmmCode) {
-		Parser parser = new Parser(new StringReader(jmmCode));
-		SimpleNode root = null; // returns reference to root node
-		try {
-			root = parser.Program();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+    public JmmParserResult parse(String jmmCode) {
+        Parser parser = new Parser(new StringReader(jmmCode));
+        SimpleNode root = null; // returns reference to root node
+        try {
+            root = parser.Program();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
-		root.dump(""); // prints the tree on the screen
+        root.dump(""); // prints the tree on the screen
 
-		return new JmmParserResult(root, new ArrayList<Report>());
-	}
+        return new JmmParserResult(root, new ArrayList<Report>());
+    }
 
     public static void main(String[] args) {
         System.out.println("Executing with args: " + Arrays.toString(args));
@@ -33,22 +33,24 @@ public class Main implements JmmParser {
             throw new RuntimeException("It's supposed to fail");
         }
 
-		InputStream in = null;
+        InputStream in = null;
 
-		try {
-			in = new FileInputStream(args[0]);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+        try {
+            in = new FileInputStream(args[0]);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-		Main main = new Main();
+        Main main = new Main();
 
-		try {
-			 main.parse();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        try {
+            main.parse();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
 
 
 }
