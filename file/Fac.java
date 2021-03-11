81966c42b6114d47ec5aec43ce2b0470939a20aa
Ruben Almeida <almeidaruben35@gmail.com>
2021-03-11 00:02:15 +0000

    Dot works

--- file/Fac.java
+++ file/Fac.java
@@ -4,6 +4,7 @@ class Fac {
     public int ComputeFac(int num) {
         int[] chars;
         int num_aux;
+        chars=new Bar();
         chars[1] = 4;
         if (num < 1)
             num_aux = 1;
@@ -11,6 +12,7 @@ class Fac {
         else
             num_aux;
             //num_aux = num * (this.ComputeFac(num - 1));
+            num_aux.test();
         return num_aux;
     }
 
