b6b517edf4d204f647dd50156f03289a44373e1c
lmarques03 <henriquecondado@gmail.com>
2021-04-08 19:33:01 +0100

    Done verification of boolean expressions

--- src/Visitors/SecondVisitor.java
+++ src/Visitors/SecondVisitor.java
@@ -14,6 +14,9 @@ public class SecondVisitor extends PreorderJmmVisitor<SecondVisitorHelper, Boole
         addVisit("Sub", this::dealWithMathOperation);
         addVisit("Mult", this::dealWithMathOperation);
         addVisit("Div", this::dealWithMathOperation);
+
+        addVisit("And", this::dealWithBooleanOperation);
+        addVisit("Not", this::dealWithBooleanOperation);
         setDefaultVisit(this::defaultVisit);
     }
 
@@ -38,6 +41,55 @@ public class SecondVisitor extends PreorderJmmVisitor<SecondVisitorHelper, Boole
 
         else if (!TypeLeft.equals(TypeRight))
             System.err.println("Not equals");
+
+
+        return true;
+    }
+
+    protected Boolean dealWithBooleanOperation(JmmNode node, SecondVisitorHelper secondVisitorHelper) {
+
+        if (node.getKind().equals("And"))
+        {
+            SeekReturnTypeVisitor seekReturnTypeVisitorLeft = new SeekReturnTypeVisitor();
+            SeekReturnTypeVisitor seekReturnTypeVisitorRight = new SeekReturnTypeVisitor();
+
+            //TODO:WHY???? NULL ON VISIT??'
+            seekReturnTypeVisitorLeft.visit(node.getChildren().get(0), secondVisitorHelper);
+            Type TypeLeft = seekReturnTypeVisitorLeft.getType();
+
+            seekReturnTypeVisitorRight.visit(node.getChildren().get(1), secondVisitorHelper);
+
+            Type TypeRight = seekReturnTypeVisitorRight.getType();
+
+            System.out.println(!TypeLeft.getName().equals("boolean"));
+            System.out.println(!TypeRight.getName().equals("boolean"));
+
+            if (TypeLeft == null)
+                System.err.println("Left child is null");
+
+            if (TypeRight == null)
+                System.err.println("Right child is null");
+
+            else if ((!TypeLeft.getName().equals("boolean")) || (!TypeRight.getName().equals("boolean")))
+                System.err.println("One or more elements are not booleans");
+
+        }
+        else {
+
+            SeekReturnTypeVisitor seekReturnTypeVisitorLeft = new SeekReturnTypeVisitor();
+
+
+            seekReturnTypeVisitorLeft.visit(node.getChildren().get(0), secondVisitorHelper);
+            Type TypeLeft = seekReturnTypeVisitorLeft.getType();
+
+            if (TypeLeft == null)
+                System.err.println("Left child is null");
+
+            else if ((!TypeLeft.getName().equals("boolean")))
+                System.err.println("One or more elements are not booleans");
+
+        }
+
 
 
         return true;
