234683fb0d0fef623ad1da00bd1bd4b39e67b246
LockdownPT <guerra70@live.com.pt>
2021-04-13 22:52:10 +0100

    OLlir class , class fields and method declaration done

--- src/stages/OptimizationStage.java
+++ src/stages/OptimizationStage.java
@@ -2,13 +2,15 @@ package stages;
 
 import pt.up.fe.comp.jmm.JmmNode;
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
-import pt.up.fe.comp.jmm.ast.examples.ExamplePreorderVisitor;
+import pt.up.fe.comp.jmm.analysis.table.Symbol;
+import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
+import pt.up.fe.comp.jmm.analysis.table.Type;
 import pt.up.fe.comp.jmm.ollir.JmmOptimization;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
 import pt.up.fe.comp.jmm.report.Report;
+import symbols.MethodSymbol;
 import symbols.SymbolTableIml;
-import visitors.OllirVisitor;
-import visitors.helpers.data_helpers.VisitorDataHelper;
+import visitors.OllirMethodVisitor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,10 +36,8 @@ public class OptimizationStage implements JmmOptimization {
         JmmNode node = semanticsResult.getRootNode();
 
         // Convert the AST to a String containing the equivalent OLLIR code
-        OllirVisitor ollirVisitor = new OllirVisitor(semanticsResult.getSymbolTable());
-        String ollirCode = ollirVisitor.visit(node, "");
-
-        System.out.println("\nOLLIR CODE\n");
+        OllirMethodVisitor ollirMethodVisitor = new OllirMethodVisitor(semanticsResult.getSymbolTable());
+        String ollirCode = ollirCodeString((SymbolTableIml) semanticsResult.getSymbolTable());
 
         System.out.println(ollirCode);
 
@@ -47,6 +47,94 @@ public class OptimizationStage implements JmmOptimization {
         // Fac {} must be replaced by ollirCode
         return new OllirResult(semanticsResult, "Fac { }", reports);
     }
+
+
+    private String ollirCodeString(SymbolTableIml symbolTable){
+        StringBuilder code = new StringBuilder();
+
+        // Create class declaration
+        code.append(symbolTable.getClassName()).append(" {\n\n");
+
+        // Create global variable declaration
+        for (Symbol variable: symbolTable.getFields()) {
+            code.append("\t").append(dealWithClassField(variable)).append("\n");;
+        }
+
+        code.append("\n");
+
+        // Create default constructor
+        code.append("\t").append(".construct ").append(symbolTable.getClassName()).append("().V{\n")
+                .append("\t\t").append("invokespecial(this, \"<init>\").V;\n")
+                .append("\t").append("}\n");
+
+
+        // Create method declaration and body
+        OllirMethodVisitor ollirMethodVisitor = new OllirMethodVisitor(symbolTable);
+        for (MethodSymbol method : symbolTable.getMethodsHashmap().values()){
+            code.append("\n\t").append(dealWithMethodHeader(method)).append(" {\n");
+            code.append(ollirMethodVisitor.visit(method.getNode(), "")).append("\n");
+            code.append("\t").append("}\n");
+        }
+
+        code.append("\n}\n");
+
+        return code.toString();
+    }
+
+
+    private String dealWithMethodHeader(MethodSymbol method){
+        StringBuilder stringBuilder = new StringBuilder();
+
+
+        stringBuilder.append(".method public ");
+
+        if(method.getName().equals("main")){
+            stringBuilder.append("static ");
+        }
+
+        stringBuilder.append(method.getName()).append(" (");
+
+        for(int i = 0; i < method.getParameters().size(); i++){
+
+            stringBuilder.append(dealWithField(method.getParameters().get(i)));
+
+            if(i != method.getParameters().size() - 1){
+                stringBuilder.append(", ");
+            }
+        }
+
+        stringBuilder.append(")").append(dealWithFieldType(method.getType()));
+
+        return stringBuilder.toString();
+    }
+
+    private String dealWithClassField(Symbol variable){
+        return ".field protected " +
+                dealWithField(variable);
+    }
+
+    private String dealWithField(Symbol variable) {
+        return variable.getName() +
+                dealWithFieldType(variable.getType());
+    }
+
+    private String dealWithFieldType(Type type) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if(type.isArray())
+            stringBuilder.append(".array");
+
+        switch (type.getName()) {
+            case "int" -> stringBuilder.append(".i32");
+            case "boolean" -> stringBuilder.append(".bool");
+            case "void" -> stringBuilder.append(".V");
+            default -> stringBuilder.append(".").append(type.getName());
+        }
+
+        return stringBuilder.toString();
+    }
+
+
 
     @Override
     public JmmSemanticsResult optimize(JmmSemanticsResult semanticsResult) {
---
+++ src/visitors/OllirMethodVisitor.java
@@ -0,0 +1,79 @@
+package visitors;
+
+import pt.up.fe.comp.jmm.JmmNode;
+import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
+import pt.up.fe.comp.jmm.ast.PreorderJmmVisitor;
+import pt.up.fe.comp.jmm.ast.examples.ExamplePreorderVisitor;
+import pt.up.fe.comp.jmm.report.Stage;
+import pt.up.fe.specs.util.utilities.StringLines;
+import utils.ReportsUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OllirMethodVisitor extends PreorderJmmVisitor<String, String> {
+
+    private SymbolTable symbolTable;
+
+    public OllirMethodVisitor(SymbolTable symbolTable) {
+        super(OllirMethodVisitor::reduce);
+
+        this.symbolTable = symbolTable;
+
+        addVisit("If", this::dealWithIf);
+        addVisit("While", this::dealWithWhile);
+        addVisit("Return", this::dealWithReturn);
+
+        setDefaultVisit(this::defaultVisit);
+    }
+
+    private String dealWithIf(JmmNode node, String code){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("\t\tif (");
+
+        stringBuilder.append(") goto else;\n");
+
+        //Code inside the if
+
+        stringBuilder.append("\t\t\tgoto endif;\n");
+        stringBuilder.append("\t\telse:\n");
+
+        //Code inside the else
+
+        stringBuilder.append("\t\tendif:");
+
+        return stringBuilder.toString();
+    }
+
+    private String dealWithWhile(JmmNode node, String code){
+        StringBuilder stringBuilder = new StringBuilder();
+        return stringBuilder.toString();
+    }
+
+    private String dealWithReturn(JmmNode node, String code){
+        StringBuilder stringBuilder = new StringBuilder();
+        return stringBuilder.toString();
+    }
+
+    private String defaultVisit(JmmNode node, String code) {
+        return code;
+    }
+
+    private static String reduce(String nodeResult, List<String> childrenResults) {
+        var content = new StringBuilder();
+
+        if(!nodeResult.equals(""))
+            content.append(nodeResult).append("\n");
+
+        for (var childResult : childrenResults) {
+            var childContent = StringLines.getLines(childResult).stream()
+                    .map(line -> " " + line + "\n")
+                    .collect(Collectors.joining());
+
+            content.append(childContent);
+        }
+        return content.toString();
+    }
+
+}
