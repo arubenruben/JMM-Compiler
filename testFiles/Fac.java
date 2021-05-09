c5613d38d6167eef3bb34877368a735a66c6a4fd
up201704618 <up201704618@fe.up.pt>
2021-05-09 10:26:40 +0100

    Fixed small bug when method call dont have parameters

--- testFiles/Fac.java
+++ testFiles/Fac.java
@@ -1,25 +1,7 @@
-class myClass {
-    public boolean check(int[] A, int N, int T) {
-        int i;
-        boolean all;
-        all = false;
-        i = 0;
-        while ((i < N) && (A[i] < T)) {
-            i = i + 1;
-        }
-        if (i < N)
-            all = true;
-        else
-            all = false;
+import ioPlus;
 
-        while ((i < N) && (A[i] < T)) {
-            i = i + 1;
-        }
-
-        while ((i < N) && (A[i] < T)) {
-            i = i + 1;
-        }
-
-        return all;
+class HelloWorld {
+    public static void main(String[] args) {
+        ioPlus.printHelloWorld();
     }
 }
